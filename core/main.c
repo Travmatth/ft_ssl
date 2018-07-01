d0eb715e7eecc33616dec52935e6fd8d1f43fb23
Travis Matthews <tmatthew@e1z3r5p20.42.us.org>
2018-07-01 14:37:19 -0700

    libft and md5 both compile

--- core/main.c
+++ core/main.c
@@ -6,11 +6,16 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 20:18:19 by tmatthew          #+#    #+#             */
-/*   Updated: 2018/06/30 20:21:03 by tmatthew         ###   ########.fr       */
+/*   Updated: 2018/07/01 12:35:38 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_ssl.h"
+
+void	ft_ssl_usage(void)
+{
+	ft_putendl("message");
+}
 
 void	ft_ssl_err(const char *message)
 {
--- md5/md5.c
+++ md5/md5.c
@@ -6,17 +6,19 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/30 20:13:01 by tmatthew          #+#    #+#             */
-/*   Updated: 2018/06/30 20:14:30 by tmatthew         ###   ########.fr       */
+/*   Updated: 2018/07/01 13:29:58 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_ssl.h"
 
-t_buf   *md5(char *digest)
+t_buf	*md5(char *digest)
 {
-    t_buf   *b;
+	t_buf	*b;
+	size_t	len;
 
-    if (!(b = ft_bufnew()))
-        ft_ssl_err("error");
-    return (b);
+	len = LEN(digest, 0);
+	if (!(b = ft_bufnew(digest, len, len)))
+		ft_ssl_err("error");
+	return (b);
 }
