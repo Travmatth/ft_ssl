ec82c5e67f172e6eaeff0cd197ac953893f3cb27
Travis Matthews <tmatthew@e1z3r2p17.42.us.org>
2018-07-07 16:10:33 -0700

    adding support for reading from stdin

--- core/hash_opts.c
+++ core/hash_opts.c
@@ -6,67 +6,140 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/03 18:50:26 by tmatthew          #+#    #+#             */
-/*   Updated: 2018/07/03 20:27:28 by tmatthew         ###   ########.fr       */
+/*   Updated: 2018/07/07 16:08:07 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_ssl.h"
 
-static void	read_from_stdin(t_digest *input)
+t_buf		*ft_bufaddspace(t_buf *b, size_t i)
 {
-	char	buf[BUFF_SIZE];
-	t_msg	digest;
-	t_buf	b;
-	int		bytes;
+	char	*tmp;
 
-	ft_bzero(buf);
-	while ((bytes = read(STDIN, buf, BUFF_SIZE)))
-		ft_bufappend(b, buf, bytes);
-	ft_memcpy(input->digests[0], b, sizeof(t_buf));
+	if (!b)
+		return (NULL);
+	if (!i)
+		return (b);
+	tmp = ft_realloc(b->buf, b->total + i);
+	b->buf = tmp;
+	b->total += i;
 }
 
-t_digest	*parse_opts(int argc, char *argv)
+char	*ft_strnjoin(char const *s1, char const *s2, size_t i)
 {
+	char	*u_str;
+	size_t	i;
+	size_t	j;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = -1;
+	j = -1;
+	s1_len = LEN(s1, 0);
+	s2_len = LEN(s2, 0);
+	if (!(u_str = ft_strnew((s1_len + s2_len))))
+		return (NULL);
+	while (++i < s1_len)
+		*(u_str + i) = *(s1 + i);
+	while (++j < s2_len && j < i)
+		*(u_str + i++) = *(s2 + j);
+	return (u_str);
+}
+
+char		*ft_str_from_fd(int fd)
+{
+	size_t	i;
+	size_t	bytes;
+	char	buf[BUFF_SIZE];
+	char	*string;
+	char	*tmp;
+
+	string = ft_memalloc(1);
+	while ((bytes = read(fd, buf, BUFF_SIZE)) != 0)
+	{
+		tmp = ft_strnjoin(string, buf, BUFF_SIZE);
+		free(string);
+		string = tmp;
+	}
+	return (string);
+}
+
+static void	read_from_stdin(t_digest *input, t_msg digest, int file)
+{
+	size_t	i;
+	char	*string;
+	char	*new_offset;
+
+	if (file && !GET_P(input->flags))
+		return ;
+	digest.msg = ft_str_from_fd(STDIN);
+	digest.type = FROM_STDIN;
+	input->digests = ft_bufaddspace(input->digests, sizeof(t_msg));
+	new_offset = (char*)input->digests + sizeof(t_msg);
+	ft_memmove((void*)new_offset, input->digests, sizeof(t_msg));
+	ft_memcpy(input->digests, &digest, sizeof(t_msg));
+}
+
+t_digest	*parse_opts(int argc, char **argv)
+{
+	int			file;
 	int			i;
+	int			fd;
 	size_t		len;
 	t_digest	*input;
 	t_msg		digest;
+	char		**args;
 
 	if (!(input = ft_memalloc(sizeof(t_digest)))
-		|| !(input->digests = ft_bufnew(ft_memalloc(0), 0, sizeof(t_msg))))
+		|| !(input->digests = ft_bufnew(ft_memalloc(1), 0, sizeof(t_msg))))
 		ft_ssl_err("error");
-	i = 0;
-	read_from_stdin(input);
-	while (i < argc)
+	i = -1;
+	args = ft_strsplit(*argv, ' ');
+	while (++i < argc)
 	{
 		ft_bzero(&digest, sizeof(t_msg));
-		if (ft_strequ("-p", argv[0]))
+		if (ft_strequ("-p", args[i]))
 		{
 			SET_P(input->flags);
-			*argv = argv[1];
+			free(args[i]);
+			ft_bufappend(input->digests, &digest, sizeof(t_msg));
 		}
-		else if (ft_strequ("-q", argv[0]))
+		else if (ft_strequ("-q", args[i]))
 		{
 			SET_Q(input->flags);
-			*argv = argv[1];
+			free(args[i]);
+			ft_bufappend(input->digests, &digest, sizeof(t_msg));
 		}
-		else if (ft_strequ("-r", argv[0]))
+		else if (ft_strequ("-r", args[i]))
 		{
 			SET_R(input->flags);
-			*argv = argv[1];
+			free(args[i]);
+			ft_bufappend(input->digests, &digest, sizeof(t_msg));
 		}
-		else if (ft_strequ("-s", argv[0]))
+		else if (ft_strequ("-s", args[i]))
 		{
-			if (!argv[1])
+			if (!args[1])
 				ft_ssl_err("error");
-			len = LEN(argv[1], 0);
-			input->digests[i].msg = ft_strsub(argv[1]
-				, *argv[1] == '"' ? 1 : 0
-				, len - (*argv[1] == '"' ? 1 : 0 ))
-			input->digests[i].type = STRING;
-			*argv = argv[2];
+			len = LEN((char*)args[1], 0);
+			digest.msg = args[i + 1];
+			digest.type = FROM_STRING;
+			free(args[i]);
+			ft_bufappend(input->digests, &digest, sizeof(t_msg));
 		}
-		ft_bufappend(input->digests, digest, sizeof(t_msg))
-		i += 1;
+		else
+		{
+			if (ERR(fd = open(args[i], O_RDONLY)))
+				ft_ssl_err(FT_MD5_NO_SUCH_FILE);
+			digest.msg = ft_str_from_fd(fd);
+			close(fd);
+			digest.type = FROM_FILE;
+			free(args[i]);
+			file = TRUE;
+		}
 	}
+	free(args);
+	read_from_stdin(input, digest, file)
+	return (input);
 }
--- ft_ssl.h
+++ ft_ssl.h
@@ -6,28 +6,36 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 12:34:43 by tmatthew          #+#    #+#             */
-/*   Updated: 2018/07/03 20:21:08 by tmatthew         ###   ########.fr       */
+/*   Updated: 2018/07/07 15:55:22 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SSL_H
 # define FT_SSL_H
 
+# include <fcntl.h>
 # include "libftprintf/srcs/includes/ft_printf.h"
 
-# define STDIN 1
-# define STRING 0
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+# define FROM_FILE 0
+# define FROM_STRING 1
+# define FROM_STDIN 2
 # define FT_SSL_FUNCS 1
+# define FT_MD5_NO_SUCH_FILE "md5: no such file"
 # define SET_P(v) (BITSET(v, 0))
 # define SET_Q(v) (BITSET(v, 1))
 # define SET_R(v) (BITSET(v, 2))
 # define SET_S(v) (BITSET(v, 3))
+// # define SET_F(v) (BITSET(v, 4))
 # define GET_P(v) (BITTEST(v, 0))
 # define GET_Q(v) (BITTEST(v, 1))
 # define GET_R(v) (BITTEST(v, 2))
 # define GET_S(v) (BITTEST(v, 3))
+// # define GET_F(v) (BITTEST(v, 4))
 
-typedef void	(*t_f)(int *argc, char **argv);
+typedef void	(*t_f)(int argc, char **argv);
 
 typedef struct	s_ptrs
 {
@@ -37,14 +45,14 @@ typedef struct	s_ptrs
 
 typedef struct	s_msg
 {
-	t_buf		*msg;
+	char		*msg;
 	char		type;
 }				t_msg;
 
 typedef struct	s_digest
 {
-	char		flags[BITNSLOTS(4)];
-	t_msg		**digests;
+	char		flags[BITNSLOTS(5)];
+	t_buf		*digests;
 }				t_digest;
 
 typedef struct	s_hash
@@ -54,5 +62,6 @@ typedef struct	s_hash
 }				t_hash;
 
 void			ft_ssl_err(const char *message) __attribute__((noreturn));
-void			md5(int *argc, char **argv);
+void			md5(int rgc, char **argv);
+t_digest		*parse_opts(int argc, char **argv);
 #endif
